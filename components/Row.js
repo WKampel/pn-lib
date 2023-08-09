64bcbef689231c0c485eaa278d8d16f10cce476c
WKampel <53668676+WKampel@users.noreply.github.com>
2023-08-09 13:03:01 +0100

    CHanges.

--- components/Row.js
+++ components/Row.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 
 const Row = ({ children, style }) => {
-  return <View style={[styles.row, style]}>{React.Children.map(children, child => React.cloneElement(child, { insideRow: true }))}</View>
+  return (
+    <View style={[styles.row, style]}>{React.Children.map(children, child => (child ? React.cloneElement(child, { insideRow: true }) : null))}</View>
+  )
 }
 
 export default Row
