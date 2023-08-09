e8e17cf70ba8072d371f3a2f7268ba577739926f
WKampel <53668676+WKampel@users.noreply.github.com>
2023-08-09 14:51:08 +0100

    Changes.

--- hooks/useNotifyServerNotifications.js
+++ hooks/useNotifyServerNotifications.js
@@ -1,12 +1,16 @@
+import { useNotification } from '../contexts/Notification'
 import { useSocketEvent } from '../contexts/Socket'
 
 const useNotifyServerNotifications = () => {
+  const { notify } = useNotification()
+
   useSocketEvent('notification', data => {
     notify({
       type: data.type,
       title: data.title,
       body: data.body,
       linkTo: data.linkTo,
+      lifeSpan: data.lifeSpan,
     })
   })
 }
