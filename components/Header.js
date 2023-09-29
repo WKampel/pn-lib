1cb68e2e1e9b5053b38ec6b70ff0926a6be41c63
WKampel <53668676+WKampel@users.noreply.github.com>
2023-09-29 12:20:36 +0100

    Fixed header on mobile web.

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = props => {
   const dimensions = useWindowDimensions()
 
   return (
-    <View style={[styles.header]}>
+    <View style={[styles.header, dimensions.width < 900 && styles.mobileHeader]}>
       {props.options?.back ? <BackButton to={props.options?.back} /> : null}
       <Text style={styles.title}>{props.options?.title || props.route.name}</Text>
       {dimensions.width < 900 ? <OpenDrawerButton /> : <View />}
@@ -19,6 +19,10 @@ export default Header
 const styles = StyleSheet.create({
   header: {
     padding: 15,
+  },
+  mobileHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   title: {
     fontSize: 22,
