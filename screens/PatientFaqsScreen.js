1e7feadd3232b45bf35268ca4396578350113e18
WKampel <53668676+WKampel@users.noreply.github.com>
2023-11-11 15:30:30 +0000

    Changes.

--- screens/PatientFaqsScreen.js
+++ screens/PatientFaqsScreen.js
@@ -1,6 +1,5 @@
 import Accordion from '../components/Accordion'
 import Card from '../components/Card'
-import Group from '../components/Group'
 import Screen from '../components/Screen'
 import useQuery from '../hooks/useQuery'
 import GET_ALL_FAQS from '../queries/GET_ALL_FAQS'
@@ -9,40 +8,15 @@ const PatientFaqsScreen = ({ data: propData }) => {
   const { data } = useQuery(GET_ALL_FAQS)
   const faqs = propData || data?.allFaqs || []
 
-  const mockFaqs = [
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-    { question: 'What do we do?', answer: 'Nothing reallt, We just make sure that you are taken acre of and whatnot.' },
-  ]
-
   return (
     <Screen>
       <Card scroll>
-        <Group>
-          <Accordion
-            items={mockFaqs.map(faq => ({
-              title: faq.question,
-              body: faq.answer,
-            }))}
-          />
-        </Group>
+        <Accordion
+          items={faqs.map(faq => ({
+            title: faq.question,
+            body: faq.answer,
+          }))}
+        />
       </Card>
     </Screen>
   )
