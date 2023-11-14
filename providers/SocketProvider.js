d357c192918f0f2b8f0f5086f7872983a50c1874
WKampel <53668676+WKampel@users.noreply.github.com>
2023-11-14 00:03:10 +0000

    Auto sync

--- providers/SocketProvider.js
+++ providers/SocketProvider.js
@@ -1,5 +1,4 @@
-import { useFocusEffect } from '@react-navigation/native'
-import React, { useCallback, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { DeviceEventEmitter } from 'react-native'
 import io from 'socket.io-client'
 import SocketContext from '../contexts/SocketContext'
@@ -9,35 +8,24 @@ const SocketProvider = ({ token, children }) => {
   const socketRef = useRef(null)
   const practice = usePractice()
 
-  useFocusEffect(
-    useCallback(() => {
-      if (!practice.id) return
+  useEffect(() => {
+    const socket = io(process.env.EXPO_PUBLIC_API_URL, {
+      query: { token, app: process.env.EXPO_PUBLIC_APP, practiceId: practice.id },
+    })
 
-      console.log('ONNING SOCKET')
+    socket.on('error', e => {
+      alert('error:' + e)
+    })
 
-      const newSocket = io(process.env.EXPO_PUBLIC_API_URL, {
-        query: { token, app: process.env.EXPO_PUBLIC_APP, practiceId: practice.id },
-      })
+    socket.onAny((event, ...args) => {
+      DeviceEventEmitter.emit('socket event', { type: event, data: args[0] })
+    })
 
-      socketRef.current = newSocket
-
-      newSocket.on('error', e => {
-        console.error('Socket error:', e)
-      })
-
-      newSocket.onAny((event, ...args) => {
-        DeviceEventEmitter.emit('socket event', { type: event, data: args[0] })
-      })
-
-      return () => {
-        newSocket.off('error')
-        newSocket.offAny()
-        newSocket.disconnect()
-
-        console.log('OFF SOCKET')
-      }
-    }, [token, practice.id])
-  )
+    return () => {
+      socket?.off('error')
+      socket?.off('new message')
+    }
+  }, [token, process.env.EXPO_PUBLIC_APP, practice?.id])
 
   return <SocketContext.Provider value={{ socket: socketRef.current }}>{children}</SocketContext.Provider>
 }
