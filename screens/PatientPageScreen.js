540f73cd8f9f27ca9206cfd619f737b0e815966a
WKampel <53668676+WKampel@users.noreply.github.com>
2023-11-14 12:54:45 +0000

    Auto sync

--- screens/PatientPageScreen.js
+++ screens/PatientPageScreen.js
@@ -2,13 +2,9 @@ import PageField from '../components/PageField'
 import Screen from '../components/Screen'
 // import useQuery from '../hooks/useQuery'
 // import GET_PAGE from '../queries/GET_PAGE'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Card from '../components/Card'
 
 const PatientPageScreen = ({ data = {} }) => {
-  // const { data: queryData } = useQuery(GET_PAGE, { variables: { id, name }, displayError: true })
-  // const data = propData || queryData?.page || {}
-
   return (
     <Screen>
       <Card scroll style={{ flexGrow: 1 }}>
