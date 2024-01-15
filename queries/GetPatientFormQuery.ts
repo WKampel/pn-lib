f61d0c91c68ef097392a7616117521c30c1f1654
WKampel <53668676+WKampel@users.noreply.github.com>
2024-01-15 18:06:09 -0500

    Changes.

--- queries/GetPatientFormQuery.ts
+++ queries/GetPatientFormQuery.ts
@@ -1,31 +1,31 @@
-import { graphql } from '../../gql'
+// import { graphql } from '../../gql'
 
-export const GetPatientForm = graphql(`
-  query GetPatientForm($id: ID) {
-    patientForm(id: $id) {
-      id
-      form {
-        name
-        desc
+// export const GetPatientForm = graphql(`
+//   query GetPatientForm($id: ID) {
+//     patientForm(id: $id) {
+//       id
+//       form {
+//         name
+//         desc
 
-        fields {
-          id
-          type
-          name
-          required
-          options {
-            id
-            value
-          }
-        }
-      }
-      responses {
-        id
-        value
-        formField {
-          id
-        }
-      }
-    }
-  }
-`)
+//         fields {
+//           id
+//           type
+//           name
+//           required
+//           options {
+//             id
+//             value
+//           }
+//         }
+//       }
+//       responses {
+//         id
+//         value
+//         formField {
+//           id
+//         }
+//       }
+//     }
+//   }
+// `)
