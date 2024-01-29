12c7243a528ed2fa392c067c6169efffce9d4c70
WKampel <53668676+WKampel@users.noreply.github.com>
2024-01-29 12:33:25 -0500

    Auto sync

--- common/components/Popup.tsx
+++ common/components/Popup.tsx
@@ -2,23 +2,32 @@ import { ReactNode } from 'react'
 import { Modal, Pressable, SafeAreaView } from 'react-native'
 import { KeyboardAvoidingView } from './KeyboardAvoidingView'
 
-export const Popup = ({ visible, onPressBackground, children }: { visible: boolean; onPressBackground?: () => void; children: ReactNode }) => {
+const PopupContent = ({ onPressBackground, children }: { onPressBackground?: () => void; children: ReactNode }) => {
+  return (
+    <KeyboardAvoidingView>
+      <Pressable
+        style={{
+          backgroundColor: 'rgba(0,0,0,.75)',
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+        }}
+        onPress={onPressBackground}
+      />
+      <SafeAreaView pointerEvents='box-none' style={{ flex: 1 }}>
+        {children}
+      </SafeAreaView>
+    </KeyboardAvoidingView>
+  )
+}
+
+// The modal has a bug on ios where it doesn't always show despite visible=true. That's why we have this component.
+export const PopupWithoutModal = PopupContent
+
+export const Popup = ({ visible, ...props }: { visible: boolean; onPressBackground?: () => void; children: ReactNode }) => {
   return (
     <Modal visible={visible} transparent={true} animationType='fade'>
-      <KeyboardAvoidingView>
-        <Pressable
-          style={{
-            backgroundColor: 'rgba(0,0,0,.75)',
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-          }}
-          onPress={onPressBackground}
-        />
-        <SafeAreaView pointerEvents='box-none' style={{ flex: 1 }}>
-          {children}
-        </SafeAreaView>
-      </KeyboardAvoidingView>
+      <PopupContent {...props} />
     </Modal>
   )
 }
--- common/contexts/MeContext.ts
+++ common/contexts/MeContext.ts
@@ -1,12 +1,21 @@
 import { createContext } from 'react'
+import { Gender } from '../../../gql/graphql'
 
+type Patient = {
+  id: string
+  firstName?: string | null
+  lastName?: string | null
+  birthDay?: Date | null
+  gender?: Gender | null
+}
 export type Me = {
   email: string
   firstName: string
   lastName: string
-  patientId?: string
-  patientFirstName?: string
-  patientLastName?: string
+  patient?: Patient | null
+  patientError?: boolean
+  patientLoading?: boolean
+  refetchPatient?: () => void
 }
 
 export const MeContext = createContext<Me | null>(null)
