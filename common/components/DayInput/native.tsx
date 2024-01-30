f979d7c23127aec0339c0aef1af7791562f77388
WKampel <53668676+WKampel@users.noreply.github.com>
2024-01-30 04:22:44 -0500

    Auto sync

--- common/components/DayInput/native.tsx
+++ common/components/DayInput/native.tsx
@@ -1,7 +1,10 @@
+import moment from 'moment'
 import { useState } from 'react'
 import { Text, View } from 'react-native'
 import DateTimePickerModal from 'react-native-modal-datetime-picker'
 import { DayInputProps } from '.'
+import { useTheme } from '../../hooks/useTheme'
+import { SolidButton } from '../buttons/SolidButton'
 
 const DayInput = ({ onChange, value, label }: DayInputProps) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -18,10 +21,19 @@ const DayInput = ({ onChange, value, label }: DayInputProps) => {
     hide()
   }
 
+  const tokens = useTheme()
+
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+    <View style={{ gap: tokens.spacing_s }}>
       <Text>{label}</Text>
-      <DateTimePickerModal isVisible={isOpen} themeVariant='light' date={value || undefined} mode='date' onCancel={handleCancel} onConfirm={handleConfirm} />
+      <View style={{ marginRight: 'auto' }}>
+        <SolidButton variant='secondary' onPress={show} text={value ? moment(value).format('MMM DD, YYYY') : 'Pick Day'} />
+      </View>
+
+      {/* This wrapping view is necessary. The DateTimePickerModal adds an extra element, and if it's not wrapped in a view that extra element with affect the gap */}
+      <View>
+        <DateTimePickerModal isVisible={isOpen} themeVariant='light' date={value || undefined} mode='date' onCancel={handleCancel} onConfirm={handleConfirm} />
+      </View>
     </View>
   )
 }
