f5bac0c32cd447df0230ac0eee5434f084c0a79a
WKampel <53668676+WKampel@users.noreply.github.com>
2024-01-30 04:22:41 -0500

    Auto sync

---
+++ queries/GetActiveAnnouncementsQuery.ts
@@ -0,0 +1,11 @@
+import { graphql } from '../../gql'
+
+export const GetActiveAnnouncements = graphql(`
+  query GetActiveAnnouncements($practiceId: ID!) {
+    activeAnnouncements(practiceId: $practiceId) {
+      id
+      text
+      expiration
+    }
+  }
+`)
