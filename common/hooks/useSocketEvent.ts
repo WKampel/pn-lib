6ea1da9d89318a1b7ba8dd7b6ee18398993d4e21
WKampel <53668676+WKampel@users.noreply.github.com>
2024-01-31 07:35:44 -0500

    Auto sync

--- common/hooks/useSocketEvent.ts
+++ common/hooks/useSocketEvent.ts
@@ -1,7 +1,12 @@
 import { useEffect, useRef } from 'react'
 import { DeviceEventEmitter } from 'react-native'
+import { AllRealTimeEvents } from '../../../pn-core-lib/types/realTimeEvents/AllRealTimeEvents'
 
-export const useSocketEvent = (type: string, callback: (data: any) => void, dependencies: Array<any> = []) => {
+export const useSocketEvent = (
+  type: keyof AllRealTimeEvents, // Use keyof AllRealTimeEvents to restrict type to valid event names
+  callback: (data: AllRealTimeEvents[keyof AllRealTimeEvents]) => void, // Use the correct data type based on the event name
+  dependencies: Array<any> = []
+) => {
   // Use a ref to track the latest callback
   const callbackRef = useRef(callback)
 
@@ -22,6 +27,4 @@ export const useSocketEvent = (type: string, callback: (data: any) => void, depe
       eventListener.remove()
     }
   }, [type, ...dependencies]) // Add dependencies that might change how the event is handled
-
-  // No need to return anything from this hook
 }
