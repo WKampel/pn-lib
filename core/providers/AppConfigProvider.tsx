3449fa164e5cbf9b3ebd16debe943f67e5b94482
WKampel <53668676+WKampel@users.noreply.github.com>
2024-01-31 08:10:57 -0500

    Auto sync

--- core/providers/AppConfigProvider.tsx
+++ core/providers/AppConfigProvider.tsx
@@ -2,9 +2,22 @@ import { ReactElement } from 'react'
 import { AppConfigContext } from '../contexts/AppConfigContext'
 import { AppType } from '../types/AppType'
 
+function getAppType(): AppType {
+  const app = process.env.EXPO_PUBLIC_APP
+
+  if (!app) {
+    throw new Error('EXPO_PUBLIC_APP is not defined')
+  }
+  if (app !== 'PATIENT' && app !== 'OFFICE') {
+    throw new Error(`App ${app} is not supported`)
+  }
+
+  return app
+}
+
 export const AppConfigProvider = ({ children }: { children: ReactElement }) => {
   const config = {
-    app: process.env.EXPO_PUBLIC_APP as AppType,
+    app: getAppType(),
   }
 
   return <AppConfigContext.Provider value={config}>{children}</AppConfigContext.Provider>
