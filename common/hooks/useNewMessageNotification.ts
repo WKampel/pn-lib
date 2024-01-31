8ac89fa4b902bf4ad33ef36cda8471fce000737c
WKampel <53668676+WKampel@users.noreply.github.com>
2024-01-31 07:55:27 -0500

    Auto sync

--- common/hooks/useNewMessageNotification.ts
+++ common/hooks/useNewMessageNotification.ts
@@ -1,18 +1,49 @@
-import { Message } from '../../../gql/graphql'
+import { RealTimeMessageEventData } from '../../../pn-core-lib/types/realTimeEvents/RealTimeMessageEventData'
+import { useAppConfig } from '../../core/hooks/useAppConfig'
 import { useNotification } from './useNotification'
 import { useSocketEvent } from './useSocketEvent'
 
-
-export const useNewMessageNotification = () => {
+export const useNewMessageNotification = ({ onPressNotification: onPressMessage }: { onPressNotification: (message: RealTimeMessageEventData) => void }) => {
   const { notify } = useNotification()
+  const config = useAppConfig()
 
-  useSocketEvent('new message', (data) => {
-    notify({
-      type: 'INFO',
-      title: `${data.} ${data} sent you a message`,
-      body: data.body,
-      linkTo: '',
-      lifeSpan: data.lifeSpan,
-    })
+  useSocketEvent('new message', data => {
+    if (config.app === 'OFFICE') {
+      if (data.sentBy === 'PATIENT') {
+        notify({
+          type: 'INFO',
+          title: `${data.patientFullName} ${data} sent you a message`,
+          body: data.body,
+          onPress: () => onPressMessage(data),
+          lifeSpan: 60,
+        })
+      } else if (data.sentBy === 'SERVER') {
+        notify({
+          type: 'INFO',
+          title: `New message`,
+          body: data.body,
+          onPress: () => onPressMessage(data),
+          lifeSpan: 60,
+        })
+      }
+    } else if (config.app === 'PATIENT') {
+      if (data.sentBy === 'ADMIN') {
+        notify({
+          type: 'INFO',
+          title: `${data.adminFullName} ${data} sent you a message`,
+          body: data.body,
+          onPress: () => onPressMessage(data),
+          lifeSpan: 60,
+        })
+      } else if (data.sentBy === 'SERVER') {
+        notify({
+          type: 'INFO',
+          title: `New message`,
+          body: data.body,
+          onPress: () => onPressMessage(data),
+          lifeSpan: 60,
+        })
+      }
+    }
   })
 }
