1954f864702a69dbb740be3f076495fbac2ae766
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-01 05:28:27 -0500

    Auto sync

--- core/hooks/useHandlePushNotifs.ts
+++ core/hooks/useHandlePushNotifs.ts
@@ -1,5 +1,7 @@
+import { NavigationProp } from '@react-navigation/native'
 import * as Notifications from 'expo-notifications'
 import { useEffect } from 'react'
+import { RootStackParamList } from '../../../core/types/navigationTypes'
 import { PushNotifData, PushNotifType } from '../../../pn-core-lib/types/PushNotifData'
 
 type PushNotifHandler<T extends PushNotifData> = (data: Extract<PushNotifData, { type: T['type'] }>) => void
@@ -8,7 +10,7 @@ type PushNotifHandlers = {
   [K in PushNotifType]: PushNotifHandler<Extract<PushNotifData, { type: K }>>
 }
 
-export const useHandlePushNotif = (handlers?: PushNotifHandlers) => {
+export const useHandlePushNotifs = (navigation: NavigationProp<RootStackParamList>, handlers?: PushNotifHandlers) => {
   useEffect(() => {
     const subscription = Notifications.addNotificationResponseReceivedListener(response => {
       const data = response.notification.request.content.data as PushNotifData
@@ -18,6 +20,7 @@ export const useHandlePushNotif = (handlers?: PushNotifHandlers) => {
       switch (data.type) {
         case 'ANNOUNCEMENT_CREATED':
           if (handlers?.[data.type]) handlers[data.type](data)
+          navigation.navigate('Practice', { id: data.announcementId })
           break
         case 'FORM_ASSIGNED':
           if (handlers?.[data.type]) handlers[data.type](data)
