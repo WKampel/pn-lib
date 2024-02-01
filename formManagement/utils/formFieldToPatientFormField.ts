baf73d2f475737b5b42b1fff8bcb9fe2dab1c27c
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-01 09:23:57 -0500

    Auto sync

--- formManagement/utils/formFieldToPatientFormField.ts
+++ formManagement/utils/formFieldToPatientFormField.ts
@@ -1,28 +1,28 @@
-import { FormField, FormFieldType } from '../../../gql/graphql'
+import { FormField, FormFieldType, FormResponse } from '../../../gql/graphql'
 import { PatientFormFieldData } from '../types/PatientFormFieldData'
 
-export function formFieldToPatientFormField(field: FormField): PatientFormFieldData {
+export function formFieldToPatientFormField(field: FormField, response?: FormResponse): PatientFormFieldData {
   const type: FormFieldType = field.type
   switch (type) {
     case 'TITLE':
       return { ...field, type: 'TITLE' as const }
     case 'TEXT_INPUT':
-      return { ...field, type: 'TEXT_INPUT' as const, value: '' }
+      return { ...field, type: 'TEXT_INPUT' as const, value: response?.textInputValue || '' }
     case 'DATE':
-      return { ...field, type: 'DATE' as const, value: null }
+      return { ...field, type: 'DATE' as const, value: response?.dateValue || null }
     case 'TIME':
-      return { ...field, type: 'TIME' as const, value: null }
+      return { ...field, type: 'TIME' as const, value: response?.timeValue || null }
     case 'YES_NO':
-      return { ...field, type: 'YES_NO' as const, value: null }
+      return { ...field, type: 'YES_NO' as const, value: response?.yesNoValue || null }
     case 'RADIO':
-      return { ...field, type: 'RADIO' as const, value: [] }
+      return { ...field, type: 'RADIO' as const, value: response?.radioValue || [] }
     case 'DROPDOWN':
-      return { ...field, type: 'DROPDOWN' as const, value: '' }
+      return { ...field, type: 'DROPDOWN' as const, value: response?.dropdownValue || '' }
     case 'LONG_TEXT':
       return { ...field, type: 'LONG_TEXT' as const }
     case 'SIGNATURE':
-      return { ...field, type: 'SIGNATURE' as const, value: '' }
+      return { ...field, type: 'SIGNATURE' as const, value: response?.signatureValue || '' }
     case 'TEXT_AREA':
-      return { ...field, type: 'TEXT_AREA' as const, value: '' }
+      return { ...field, type: 'TEXT_AREA' as const, value: response?.textAreaValue || '' }
   }
 }
