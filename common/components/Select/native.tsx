571ee5a2776c0b58e51f77217889343cfe0d5256
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-01 10:13:30 -0500

    Auto sync

--- common/components/Select/native.tsx
+++ common/components/Select/native.tsx
@@ -1,7 +1,78 @@
+import { Picker } from '@react-native-picker/picker'
+import { useState } from 'react'
+import { Modal, Pressable, Text, TouchableOpacity, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { SelectProps } from '.'
 
-const Select = <TOption extends any, TValue extends string>(props: SelectProps<TOption, TValue>) => {
-  return <></>
+const Select = <TOption extends any, TValue extends string>({ value, onChange, options, getLabel, getValue, label = 'Select' }: SelectProps<TOption, TValue>) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const selectedOption = options.find(option => getValue(option) === value)
+  let selectedLabel = ''
+
+  if (selectedOption) {
+    selectedLabel = getLabel(selectedOption)
+  }
+
+  const insets = useSafeAreaInsets()
+
+  return (
+    <View
+      style={{
+        alignSelf: 'flex-start',
+        gap: 10,
+      }}
+    >
+      <Text
+        style={{
+          color: 'black',
+        }}
+      >
+        {label}
+      </Text>
+
+      <View>
+        <TouchableOpacity
+          style={{
+            backgroundColor: 'rgb(230,230,230)',
+            alignSelf: 'flex-start',
+            paddingVertical: 12,
+            paddingHorizontal: 14,
+            borderRadius: 8,
+          }}
+          onPress={() => setIsOpen(true)}
+        >
+          <Text
+            style={{
+              color: 'black',
+            }}
+          >
+            {selectedLabel || 'Select'}
+          </Text>
+        </TouchableOpacity>
+
+        <Modal onRequestClose={() => setIsOpen(false)} visible={isOpen} transparent={true} animationType='fade'>
+          <Pressable
+            pointerEvents='box-only'
+            style={{ flex: 1, backgroundColor: 'rgba(0,0,0,.25)', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+            onPress={() => setIsOpen(false)}
+          />
+
+          <View style={{ marginTop: 'auto', padding: 10, paddingBottom: insets.bottom }}>
+            <View style={{ backgroundColor: 'white', borderRadius: 15 }}>
+              <Picker selectedValue={value} onValueChange={(itemValue, itemIndex) => onChange(itemValue)}>
+                {options.map(option => {
+                  const label = getLabel(option)
+                  const value = getValue(option)
+                  return <Picker.Item key={value} label={label} value={value} />
+                })}
+              </Picker>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </View>
+  )
 }
 
 export default Select
--- common/components/TimeInput/native.tsx
+++ common/components/TimeInput/native.tsx
@@ -1,22 +1,39 @@
-import DateTimePicker from '@react-native-community/datetimepicker'
 import moment from 'moment'
+import { useState } from 'react'
 import { Text, View } from 'react-native'
+import DateTimePickerModal from 'react-native-modal-datetime-picker'
 import { TimeInputProps } from '.'
+import { useTheme } from '../../hooks/useTheme'
+import { SolidButton } from '../buttons/SolidButton'
 
 const TimeInput = ({ onChange, value, label }: TimeInputProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const show = () => setIsOpen(true)
+  const hide = () => setIsOpen(false)
+
+  const handleConfirm = (date: Date) => {
+    onChange(date)
+    hide()
+  }
+
+  const handleCancel = () => {
+    hide()
+  }
+
+  const tokens = useTheme()
+
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+    <View style={{ gap: tokens.spacing_s }}>
       <Text>{label}</Text>
-      <DateTimePicker
-        style={{ padding: 0, margin: 0 }}
-        themeVariant='light'
-        value={moment(value).toDate()}
-        mode='time'
-        onChange={(e, date) => {
-          if (date) onChange(date)
-        }}
-        timeZoneOffsetInMinutes={0}
-      />
+      <View style={{ marginRight: 'auto' }}>
+        <SolidButton variant='secondary' onPress={show} text={value ? moment(value).format('hh:mm A') : 'Pick Time'} />
+      </View>
+
+      {/* This wrapping view is necessary. The DateTimePickerModal adds an extra element, and if it's not wrapped in a view that extra element with affect the gap */}
+      <View>
+        <DateTimePickerModal isVisible={isOpen} themeVariant='light' date={value || undefined} mode='time' onCancel={handleCancel} onConfirm={handleConfirm} />
+      </View>
     </View>
   )
 }
