3ba61b9a9d86fc8aad8de28f7b7f0bb83f782c4b
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-02 08:37:21 -0500

    Auto sync

--- patientManagement/components/patientForm/fields/PatientFormFieldRadio.tsx
+++ patientManagement/components/patientForm/fields/PatientFormFieldRadio.tsx
@@ -1,7 +1,7 @@
+import { FormFieldOption } from '../../../../../gql/graphql'
 import { Radio } from '../../../../common/components/Radio'
-import { FormFieldOptionData } from '../../../../formManagement/types/FormFieldOptionData'
 import { ExtractPatientFormFieldProps } from '../PatientFormFieldRenderer'
 
 export const PatientFormFieldRadio = ({ value, onChange, name, options }: ExtractPatientFormFieldProps<'RADIO'>) => {
-  return <Radio<FormFieldOptionData> value={value} onChange={onChange} label={name} options={options} getLabel={option => option.value} getValue={option => option.value} />
+  return <Radio<FormFieldOption> value={value} onChange={onChange} label={name} options={options} getLabel={option => option.value} getValue={option => option.value} />
 }
--- patientManagement/components/patientForm/fields/PatientFormFieldSelect.tsx
+++ patientManagement/components/patientForm/fields/PatientFormFieldSelect.tsx
@@ -1,16 +1,9 @@
+import { FormFieldOption } from '../../../../../gql/graphql'
 import { Select } from '../../../../common/components/Select'
-import { FormFieldOptionData } from '../../../../formManagement/types/FormFieldOptionData'
 import { ExtractPatientFormFieldProps } from '../PatientFormFieldRenderer'
 
 export const PatientFormFieldSelect = ({ value, onChange, name, options }: ExtractPatientFormFieldProps<'DROPDOWN'>) => {
   return (
-    <Select<FormFieldOptionData, string>
-      value={value}
-      onChange={onChange}
-      label={name}
-      options={options}
-      getLabel={option => option.value}
-      getValue={option => option.value}
-    />
+    <Select<FormFieldOption, string> value={value} onChange={onChange} label={name} options={options} getLabel={option => option.value} getValue={option => option.value} />
   )
 }
