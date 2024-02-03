8aee4a7b20ba0fecb3f98893ecd513708feb3c04
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-03 17:04:39 -0500

    Auto sync

--- common/components/Link.tsx
+++ common/components/Link.tsx
@@ -16,6 +16,7 @@ export const Link = ({ children, onPress }: LinkProps) => {
         style={{
           color: tokens.color_ui_primary,
           fontSize: tokens.font_size_m,
+          textDecorationLine: 'underline',
         }}
       >
         {children}
