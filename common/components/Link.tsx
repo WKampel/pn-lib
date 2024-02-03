d34f441e0be3d0aba2b204c7c27919720d48a9a3
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-03 17:06:00 -0500

    Auto sync

--- common/components/Link.tsx
+++ common/components/Link.tsx
@@ -1,23 +1,27 @@
 import React, { ReactNode } from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import { StyleProp, Text, TextStyle, TouchableOpacity } from 'react-native'
 import { useTheme } from '../hooks/useTheme'
 
 type LinkProps = {
   children: ReactNode
   onPress: () => void
+  textStyle: StyleProp<TextStyle>
 }
 
-export const Link = ({ children, onPress }: LinkProps) => {
+export const Link = ({ textStyle = {}, children, onPress }: LinkProps) => {
   const tokens = useTheme()
 
   return (
     <TouchableOpacity onPress={onPress}>
       <Text
-        style={{
-          color: tokens.color_ui_primary,
-          fontSize: tokens.font_size_m,
-          textDecorationLine: 'underline',
-        }}
+        style={[
+          {
+            color: tokens.color_ui_primary,
+            fontSize: tokens.font_size_m,
+            textDecorationLine: 'underline',
+          },
+          textStyle,
+        ]}
       >
         {children}
       </Text>
