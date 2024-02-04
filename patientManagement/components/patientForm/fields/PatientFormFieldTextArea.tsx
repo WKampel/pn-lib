9d09573db375623f37273d1dd4b872193a0f5a0f
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-04 13:35:43 -0500

    Auto sync

--- patientManagement/components/patientForm/fields/PatientFormFieldTextArea.tsx
+++ patientManagement/components/patientForm/fields/PatientFormFieldTextArea.tsx
@@ -2,5 +2,5 @@ import { TextArea } from '../../../../common/components/TextArea'
 import { ExtractPatientFormFieldProps } from '../PatientFormFieldRenderer'
 
 export const PatientFormFieldTextArea = ({ value, onChange, name }: ExtractPatientFormFieldProps<'TEXT_AREA'>) => {
-  return <TextArea value={value} onChange={onChange} label={name} />
+  return <TextArea value={value} onChange={onChange} label='' />
 }
--- patientManagement/components/patientForm/fields/PatientFormFieldTime.tsx
+++ patientManagement/components/patientForm/fields/PatientFormFieldTime.tsx
@@ -2,5 +2,5 @@ import { TimeInput } from '../../../../common/components/TimeInput'
 import { ExtractPatientFormFieldProps } from '../PatientFormFieldRenderer'
 
 export const PatientFormFieldTime = ({ value, onChange, name }: ExtractPatientFormFieldProps<'TIME'>) => {
-  return <TimeInput value={value} onChange={onChange} label={name} />
+  return <TimeInput value={value} onChange={onChange} label='' />
 }
