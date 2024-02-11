74a45137b883506eddc4a5ab363df78bc626af4b
WKampel <53668676+WKampel@users.noreply.github.com>
2024-02-11 16:51:05 -0500

    Auto sync

--- patientManagement/components/patientForm/fields/PatientFormFieldDate.tsx
+++ patientManagement/components/patientForm/fields/PatientFormFieldDate.tsx
@@ -1,6 +1,15 @@
+import moment from 'moment'
+import React from 'react'
 import { DayInput } from '../../../../common/components/DayInput'
 import { ExtractPatientFormFieldProps } from '../PatientFormFieldRenderer'
 
 export const PatientFormFieldDate = ({ value, onChange, name }: ExtractPatientFormFieldProps<'DATE'>) => {
-  return <DayInput value={value} onChange={onChange} label='' />
+  const stringValue = value ? moment.utc(value).format('YYYY-MM-DD') : ''
+
+  const handleChange = (dateString: string) => {
+    const newValue = dateString ? moment.utc(dateString, 'YYYY-MM-DD').toDate() : null
+    onChange(newValue)
+  }
+
+  return <DayInput value={stringValue} onChange={handleChange} label='' />
 }
--- patientManagement/components/patientForm/fields/PatientFormFieldTime.tsx
+++ patientManagement/components/patientForm/fields/PatientFormFieldTime.tsx
@@ -1,6 +1,21 @@
+import moment from 'moment'
+import React from 'react'
 import { TimeInput } from '../../../../common/components/TimeInput'
 import { ExtractPatientFormFieldProps } from '../PatientFormFieldRenderer'
 
 export const PatientFormFieldTime = ({ value, onChange, name }: ExtractPatientFormFieldProps<'TIME'>) => {
-  return <TimeInput value={value} onChange={onChange} label='' />
+  const stringValue = value ? moment.utc(value).local().format('hh:mm A') : ''
+
+  const handleChange = (timeString: string) => {
+    const newValue = timeString ? moment().format('YYYY-MM-DD') : null
+    if (newValue) {
+      const dateTimeString = `${newValue} ${timeString}`
+      const utcValue = moment(dateTimeString, 'YYYY-MM-DD hh:mm A').utc().toDate()
+      onChange(utcValue)
+    } else {
+      onChange(null)
+    }
+  }
+
+  return <TimeInput value={stringValue} onChange={handleChange} label='' />
 }
